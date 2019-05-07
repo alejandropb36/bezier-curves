8a315f277e53340857f7b3adfd076df76d0c461c
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-05-07 13:50:15 -0500

    Curva de grado 3 con allas en el dibujo

--- bezier-curves/MainForm.cs
+++ bezier-curves/MainForm.cs
@@ -85,7 +85,7 @@ namespace bezier_curves
             }
             else if (Convert.ToInt32(comboBox1.Text) == 4)
             {
-                //CalcularCurvasGrado3(puntos, DistanciaMayor(puntos, numeroPuntos));
+                CalcularCurvasGrado3(puntos, DistanciaMayor(puntos, numeroPuntos));
             }
             else if(Convert.ToInt32(comboBox1.Text) == 5)
             {
@@ -119,12 +119,18 @@ namespace bezier_curves
             punto1Y = (int)Math.Round((puntos[1].Y - puntos[0].Y) * tolerancia) + puntos[0].Y;
             punto2X = (int)Math.Round((puntos[2].X - puntos[1].X) * tolerancia) + puntos[1].X;
             punto2Y = (int)Math.Round((puntos[2].Y - puntos[1].Y) * tolerancia) + puntos[1].Y;
+
             resultado.X = (int)Math.Round(((punto2X - punto1X) * tolerancia) + punto1X);
             resultado.Y = (int)Math.Round(((punto2Y - punto1Y) * tolerancia) + punto1Y);
 
             WorkSpace.CreateGraphics().DrawLine(GetPen(Color.Red), punto1X, punto1Y, punto2X, punto2Y);
-            punto1 = new Point(punto1X, punto1Y);
-            punto2 = new Point(punto2X, punto2Y);
+
+            punto1.X = punto1X;
+            punto1.Y = punto1Y;
+
+            punto2.X = punto2X;
+            punto2.Y = punto2Y;
+
             return resultado;
         }
 
@@ -136,5 +142,67 @@ namespace bezier_curves
             }
         }
 
+        void CalcularCurvasGrado3(Point[] puntos, int numeroPuntos)
+        {
+            Point[] curva = new Point[numeroPuntos];
+            float tolerancia;
+            tolerancia = (float)1 / (float)(numeroPuntos - 1);
+
+            for (int i = 0; i < numeroPuntos; i++)
+            {
+                WorkSpace.CreateGraphics().DrawLine(GetPen(Color.White), punto1.X, punto1.Y, punto2.X, punto2.Y);
+                WorkSpace.CreateGraphics().DrawLine(GetPen(Color.White), punto2.X, punto2.Y, punto3.X, punto3.Y);
+                WorkSpace.CreateGraphics().DrawLine(GetPen(Color.White), punto4.X, punto4.Y, punto5.X, punto5.Y);
+
+                curva[i] = GetPuntoCurvaGrado3(puntos, (float)i * tolerancia);
+                WorkSpace.CreateGraphics().DrawLines(GetPen(Color.Black), points);
+                DibujarCurvaActual(curva, i, Color.Blue);
+
+            }
+        }
+
+        Point GetPuntoCurvaGrado3(Point[] puntos, float tolerancia)
+        {
+            Point resultado = new Point();
+            int punto1X, punto1Y;
+            int punto2X, punto2Y;
+            int punto3X, punto3Y;
+            int punto4X, punto4Y;
+            int punto5X, punto5Y;
+
+            punto1X = (int)Math.Round((puntos[1].X - puntos[0].X) * tolerancia) + puntos[0].X;
+            punto1Y = (int)Math.Round((puntos[1].Y - puntos[0].Y) * tolerancia) + puntos[0].Y;
+            punto2X = (int)Math.Round((puntos[2].X - puntos[1].X) * tolerancia) + puntos[1].X;
+            punto2Y = (int)Math.Round((puntos[2].Y - puntos[1].Y) * tolerancia) + puntos[1].Y;
+            punto3X = (int)Math.Round((puntos[3].X - puntos[2].X) * tolerancia) + puntos[2].X;
+            punto3Y = (int)Math.Round((puntos[3].Y - puntos[2].Y) * tolerancia) + puntos[2].Y;
+            punto4X = (int)Math.Round((punto2X - punto1X) * tolerancia) + punto1X;
+            punto4Y = (int)Math.Round((punto2Y - punto1Y) * tolerancia) + punto1Y;
+            punto5X = (int)Math.Round((punto3X - punto2X) * tolerancia) + punto2X;
+            punto5Y = (int)Math.Round((punto3Y - punto2Y) * tolerancia) + punto2Y;
+
+            /* Punto en la curva*/
+            resultado.X = (int)Math.Round(((punto5X - punto4X) * tolerancia) + punto4X);
+            resultado.Y = (int)Math.Round(((punto5Y - punto4Y) * tolerancia) + punto4Y);
+
+            WorkSpace.CreateGraphics().DrawLine(GetPen(Color.Red), punto4X, punto4Y, punto5X, punto5Y);
+
+            punto1.X = punto1X;
+            punto1.Y = punto1Y;
+
+            punto2.X = punto2X;
+            punto2.Y = punto2Y;
+
+            punto3.X = punto3X;
+            punto3.Y = punto3Y;
+
+            punto4.X = punto4X;
+            punto4.Y = punto4Y;
+
+            punto5.X = punto5X;
+            punto5.Y = punto5Y;
+
+            return resultado;
+        }
     }
 }
