34dc4e4ef8fcdf3b281c8abbbd4c6d713d033a27
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-05-07 23:05:27 -0500

    Curva Bezier matricial: Aun tiene problemas

--- bezier-curves/MainForm.cs
+++ bezier-curves/MainForm.cs
@@ -72,14 +72,17 @@ namespace bezier_curves
             if (Convert.ToInt32(comboBox1.Text) == 3)
             {
                 CalcularCurvasGrado2(puntos, DistanciaMayor(puntos, numeroPuntos));
+                CalcularCurvasGrado2Matricial(puntos, DistanciaMayor(puntos, numeroPuntos));
             }
             else if (Convert.ToInt32(comboBox1.Text) == 4)
             {
                 CalcularCurvasGrado3(puntos, DistanciaMayor(puntos, numeroPuntos));
+                //CalcularCurvasGrado3Matricial(puntos, DistanciaMayor(puntos, numeroPuntos));
             }
             else if(Convert.ToInt32(comboBox1.Text) == 5)
             {
                 CalcularCurvasGrado4(puntos, DistanciaMayor(puntos, numeroPuntos));
+                //CalcularCurvasGrado4Matricial(puntos, DistanciaMayor(puntos, numeroPuntos));
             }
         }
 
@@ -105,6 +108,7 @@ namespace bezier_curves
             punto1 = GetPunto(puntos[0], puntos[1], tolerancia);
             punto2 = GetPunto(puntos[1], puntos[2], tolerancia);
 
+            /* Punto en la curva*/
             resultado = GetPunto(punto1, punto2, tolerancia);
 
             WorkSpace.CreateGraphics().DrawLine(GetPen(Color.Red), punto1, punto2);
@@ -217,6 +221,117 @@ namespace bezier_curves
             WorkSpace.CreateGraphics().DrawLine(GetPen(Color.Blue), punto8, punto9);
 
             return resultado;
+        }
+
+        Point GetPuntoCurvaMatricial(Point[] puntos, float u)
+        {
+            Point Resultado = new Point();
+            float ax, bx, cx, ay, by, cy, x1, x2, x3, y1, y2, y3, u2;
+            u2 = u * u;
+            ax = puntos[0].X;
+            bx = puntos[1].X;
+            cx = puntos[2].X;
+            ay = puntos[0].Y;
+            by = puntos[1].Y;
+            cy = puntos[2].Y;
+            x1 = ax + (-2 * bx) + cx;
+            x2 = (-2 * ax) + (2 * bx);
+            x3 = ax;
+            y1 = ay + (-2 * by) + cy;
+            y2 = (-2 * ay) + (2 * by);
+            y3 = ay;
+
+            Resultado.X = (int)Math.Round((x1 * u2) + (x2 * u) + x3);
+            Resultado.Y = (int)Math.Round((y1 * u2) + (y2 * u) + y3);
+            return Resultado;
+
+
+        } 
+
+        void CalcularCurvasGrado2Matricial(Point[] puntos, int numeroPuntos)
+        {
+            Point[] curva = new Point[numeroPuntos];
+            float tolerancia;
+            tolerancia = (float)1 / (float)(numeroPuntos - 1);
+
+            for (int i = 0; i < numeroPuntos; i++)
+            {
+                curva[i] = (GetPuntoCurvaMatricial(puntos, (float)i * tolerancia));
+            }
+
+            DibujarCurvaBezier(curva, numeroPuntos - 1, Color.Red);
+
+
+        Point GetPuntoQ(Point cp, Point cp2, float u)
+        {
+            Point Resultado = new Point();
+            int Qx, Qy;
+            Qx = (int)Math.Round(cp.X + u * (cp2.X - cp.X));
+
+            Qy = (int)Math.Round(cp.Y + u * (cp2.Y - cp.Y));
+            Resultado = new Point(Qx, Qy);
+            return Resultado;
+        }
+
+
+        void CalcularCurvasGrado3Matricial(Point[] cp, int np)
+        {
+            Point[] curve = new Point[np];
+            Point[] curved2 = new Point[2];
+            Point[] curved3 = new Point[2];
+            Point[] curved4 = new Point[2];
+            Point[] curvepuntos = new Point[3];
+
+            float t;
+            t = (float)1 / (float)(np - 1);
+
+            for (int i = 0; i < np; i++)
+            {
+
+                curvepuntos[0] = GetPuntoQ(cp[0], cp[1], (float)i * t);
+                curvepuntos[1] = GetPuntoQ(cp[1], cp[2], (float)i * t);
+                curvepuntos[2] = GetPuntoQ(cp[2], cp[3], (float)i * t);
+
+                curve[i] = (GetPuntoCurvaMatricial(curvepuntos, (float)i * t));
+
+            }
+
+            DibujarCurvaBezier(curve, np - 1, Color.Red);
+        }
+
+
+
+        void CalcularCurvasGrado4Matricial(Point[] cp2, int np)
+        {
+            Point[] curve = new Point[np];
+            Point[] curved2 = new Point[2];
+            Point[] curved3 = new Point[2];
+            Point[] curved4 = new Point[2];
+            Point[] curvepuntos = new Point[3];
+            Point[] cp = new Point[4];
+
+            float t;
+            t = (float)1 / (float)(np - 1);
+
+            for (int i = 0; i < np; i++)
+            {
+                cp[0] = GetPuntoQ(cp2[0], cp2[1], (float)i * t);
+                cp[1] = GetPuntoQ(cp2[1], cp2[2], (float)i * t);
+                cp[2] = GetPuntoQ(cp2[2], cp2[3], (float)i * t);
+                cp[3] = GetPuntoQ(cp2[3], cp2[4], (float)i * t);
+
+
+                curvepuntos[0] = GetPuntoQ(cp[0], cp[1], (float)i * t);
+
+                curvepuntos[1] = GetPuntoQ(cp[1], cp[2], (float)i * t);
+
+                curvepuntos[2] = GetPuntoQ(cp[2], cp[3], (float)i * t);
+
+
+                curve[i] = (GetPuntoCurvaMatricial(curvepuntos, (float)i * t));
+            }
+
+            DibujarCurvaBezier(curve, np - 1, Color.Red);
         }
     }
 }
